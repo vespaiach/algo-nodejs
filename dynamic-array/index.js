8b8f32bea8b138346dad72105a3c8becc27c57ba
Toan Nguyen <toannguyen@Toans-MacBook-Air.local>
2025-04-15 18:18:43 -0400

    implement

--- dynamic-array/index.js
+++ dynamic-array/index.js
@@ -1,3 +1,147 @@
 export default class DynamicArray {
+  __base
+  #_size = 0 // This private field stores the size of the dynamic array
+  #_capacity = 1 // This private field stores the capacity of the dynamic array
 
+  constructor() {
+    this.__base = this.#createAndInitArray(this.#_capacity)
+  }
+
+  append(x) {
+    this.#ensureNumber(x)
+
+    if (this.#_size >= this.#_capacity) {
+      this.#doubleArrayCapacity()
+    }
+    this.__base[this.#_size] = x
+    this.#_size++
+  }
+
+  #doubleArrayCapacity() {
+    this.#_capacity *= 2
+    const tempArray = this.#createAndInitArray(this.#_capacity)
+    for (let i = 0; i < this.#_size; i++) {
+      tempArray[i] = this.__base[i]
+    }
+    this.__base = tempArray
+  }
+
+  get(i) {
+    if (i < 0 || i >= this.#_size) {
+      return undefined
+    }
+    return this.__base[i]
+  }
+
+  set(i, x) {
+    this.#ensureNumber(x)
+
+    if (i < 0 || i >= this.#_size) {
+      throw new Error('Index out of bounds')
+    }
+    this.__base[i] = x
+  }
+
+  size() {
+    return this.#_size
+  }
+
+  popBack() {
+    if (this.#_size === 0) {
+      throw new Error('Array is empty')
+    }
+    const lastElement = this.__base[this.#_size - 1]
+    this.__base[this.#_size - 1] = undefined // Clear the last element
+    this.#_size--
+
+    this.#shrinkArrayIfNeeded()
+
+    return lastElement
+  }
+
+  pop(i) {
+    if (i < 0 || i >= this.#_size) {
+      throw new Error('Index out of bounds')
+    }
+    const element = this.__base[i]
+    for (let j = i; j < this.#_size - 1; j++) {
+      this.__base[j] = this.__base[j + 1]
+    }
+    this.__base[this.#_size - 1] = undefined // Clear the last element
+    this.#_size--
+
+    this.#shrinkArrayIfNeeded()
+
+    return element
+  }
+
+  contains(x) {
+    this.#ensureNumber(x)
+    for (let i = 0; i < this.#_size; i++) {
+      if (this.__base[i] === x) {
+        return true
+      }
+    }
+    return false
+  }
+
+  insert(i, x) {
+    this.#ensureNumber(x)
+    if (i < 0 || i > this.#_size) {
+      throw new Error('Index out of bounds')
+    }
+    if (this.#_size + 1 > this.#_capacity) {
+      this.#doubleArrayCapacity()
+    }
+
+    let nextValue = x
+    for (let j = i; j < this.#_size; j++) {
+      const tempValue = this.__base[j]
+      this.__base[j] = nextValue
+      nextValue = tempValue
+    }
+    this.__base[this.#_size] = nextValue
+    this.#_size++
+  }
+
+	remove(x) {
+		this.#ensureNumber(x)
+		let foundIndex
+		for(let i = 0; i < this.#_size; i++) {
+			if (this.__base[i] === x) {
+				foundIndex = i;
+				break;
+			}
+		}
+
+		if (foundIndex !== undefined) {
+			this.pop(foundIndex)
+		}
+	}
+
+  #ensureNumber(i) {
+    if (typeof i !== 'number') {
+      throw new Error('Value must be a number')
+    }
+  }
+
+  #createAndInitArray(size) {
+    const tempArray = new Array(size)
+    for (let i = 0; i < size; i++) {
+      tempArray[i] = undefined
+    }
+    return tempArray
+  }
+
+  #shrinkArrayIfNeeded() {
+    const twentyFivePercent = Math.floor(this.#_capacity * 0.25)
+    if (twentyFivePercent && this.#_size <= twentyFivePercent) {
+      this.#_capacity /= 2
+      const tempArray = this.#createAndInitArray(this.#_capacity)
+      for (let i = 0; i < this.#_size; i++) {
+        tempArray[i] = this.__base[i]
+      }
+      this.__base = tempArray
+    }
+  }
 }
--- dynamic-array/index.test.js
+++ dynamic-array/index.test.js
@@ -1,12 +1,20 @@
-import { test, expect, describe } from 'node:test'
+import { test, describe } from 'node:test'
+import assert from 'node:assert'
+import DynamicArray from './index.js'
 
 describe('Dynamic Array - Basic Operations', () => {
   test('append(x) - should add an element to the end of the array', () => {
     const dArray = new DynamicArray()
     dArray.append(1)
-    expect(dArray.__base).toBe([1])
+    assert.deepEqual(dArray.__base, [1])
     dArray.append(2)
-    expect(dArray.__base).toBe([1, 2])
+    assert.deepEqual(dArray.__base, [1, 2])
+    dArray.append(3)
+    assert.deepEqual(dArray.__base, [1, 2, 3, undefined])
+    dArray.append(4)
+    assert.deepEqual(dArray.__base, [1, 2, 3, 4])
+    dArray.append(4)
+    assert.deepEqual(dArray.__base, [1, 2, 3, 4, 4, undefined, undefined, undefined])
   })
 
   test('get(i) - should return the element at index i', () => {
@@ -14,16 +22,17 @@ describe('Dynamic Array - Basic Operations', () => {
     dArray.append(3)
     dArray.append(2)
     dArray.append(1)
-    expect(dArray.get(0)).toBe(3)
-    expect(dArray.get(1)).toBe(2)
-    expect(dArray.get(2)).toBe(1)
+
+    assert.equal(dArray.get(0), 3)
+    assert.equal(dArray.get(1), 2)
+    assert.equal(dArray.get(2), 1)
   })
 
   test('get(i) - should return undefined if index is out of bounds', () => {
     const dArray = new DynamicArray()
     dArray.append(3)
-    expect(dArray.get(0)).toBe(3)
-    expect(dArray.get(1)).toBeUndefined()
+    assert.strictEqual(dArray.get(0), 3)
+    assert.strictEqual(dArray.get(1), undefined)
   })
 
   test('set(i, x) - should set the element at index i to x', () => {
@@ -32,28 +41,34 @@ describe('Dynamic Array - Basic Operations', () => {
     dArray.append(2)
     dArray.append(3)
     dArray.set(1, 5)
-    expect(dArray.__base).toBe([1, 5, 3])
+    assert.deepEqual(dArray.__base, [1, 5, 3, undefined])
   })
 
   test('set(i, x) - should throw an error if index is out of bounds', () => {
     const dArray = new DynamicArray()
+    try {
+      dArray.set(2, 5)
+    } catch (e) {
+      assert.strictEqual(e instanceof Error, true)
+    }
+
     dArray.append(1)
     try {
       dArray.set(2, 5)
     } catch (e) {
-      expect(e).toBeInstanceOf(Error)
+      assert.strictEqual(e instanceof Error, true)
     }
   })
 
   test('size() - should return the number of elements in the array', () => {
     const dArray = new DynamicArray()
-    expect(dArray.size()).toBe(0)
+    assert.strictEqual(dArray.size(), 0)
     dArray.append(1)
-    expect(dArray.size()).toBe(1)
+    assert.strictEqual(dArray.size(), 1)
     dArray.append(2)
-    expect(dArray.size()).toBe(2)
+    assert.strictEqual(dArray.size(), 2)
     dArray.append(3)
-    expect(dArray.size()).toBe(3)
+    assert.strictEqual(dArray.size(), 3)
   })
 
   test('popBack() - should remove the last element from the array and return it', () => {
@@ -61,68 +76,127 @@ describe('Dynamic Array - Basic Operations', () => {
     dArray.append(1)
     dArray.append(2)
     dArray.append(3)
-    expect(dArray.popBack()).toBe(3)
-    expect(dArray.__base).toBe([1, 2])
+    assert.equal(dArray.popBack(), 3)
+    assert.deepEqual(dArray.__base, [1, 2, undefined, undefined])
   })
 
-	test('popBack() - should return undefined if the array is empty', () => {
-		const dArray = new DynamicArray()
-		expect(dArray.popBack()).toBeUndefined()
-	})
+  test('popBack() - should throw an error if the array is empty', () => {
+    const dArray = new DynamicArray()
+    try {
+      dArray.popBack()
+    } catch (e) {
+      assert.equal(e instanceof Error, true)
+    }
+  })
 
-	test('pop(i) - should remove the element at index i and return it', () => {
-		const dArray = new DynamicArray()
-		dArray.append(1)
-		dArray.append(2)
-		dArray.append(3)
-		expect(dArray.pop(1)).toBe(2)
-		expect(dArray.__base).toBe([1, 3])
-	})
+  test('popBack() - should shrink the underline array to save space', () => {
+    const dArray = new DynamicArray()
+    for (let i = 0; i < 8; i++) {
+      dArray.append(i + 1)
+    }
+    assert.deepEqual(dArray.__base, [1, 2, 3, 4, 5, 6, 7, 8])
+    for (let i = 0; i < 6; i++) {
+      dArray.popBack()
+    }
+    assert.deepEqual(dArray.__base, [1, 2, undefined, undefined])
+  })
 
-	test('pop(i) - should throw an error if index is out of bounds', () => {
-		const dArray = new DynamicArray()
-		dArray.append(1)
-		try {
-			dArray.pop(2)
-		} catch (e) {
-			expect(e).toBeInstanceOf(Error)
-		}
-	})
+  test('pop(i) - should remove the element at index i and return it', () => {
+    const dArray = new DynamicArray()
+    dArray.append(1)
+    dArray.append(2)
+    dArray.append(3)
+    dArray.append(4)
+    assert.equal(dArray.pop(1), 2)
+    assert.deepEqual(dArray.__base, [1, 3, 4, undefined])
+    assert.equal(dArray.pop(0), 1)
+    assert.deepEqual(dArray.__base, [3, 4, undefined, undefined])
+    assert.equal(dArray.pop(1), 4)
+    assert.deepEqual(dArray.__base, [3, undefined])
+    assert.equal(dArray.pop(0), 3)
+    assert.deepEqual(dArray.__base, [undefined, undefined])
+  })
 
-	test('contains(x) - should return true if the array contains x, false otherwise', () => {
-		const dArray = new DynamicArray()
-		dArray.append(1)
-		dArray.append(2)
-		dArray.append(3)
-		expect(dArray.contains(2)).toBe(true)
-		expect(dArray.contains(4)).toBe(false)
-	})
+  test('pop(i) - should throw an error if index is out of bounds', () => {
+    const dArray = new DynamicArray()
+    try {
+      dArray.pop(2)
+    } catch (e) {
+      assert.equal(e instanceof Error, true)
+    }
 
-	test('insert(i, x) - should insert x at index i', () => {
-		const dArray = new DynamicArray()
-		dArray.append(1)
-		dArray.append(3)
-		dArray.insert(1, 20)
-		expect(dArray.__base).toBe([1, 20, 3])
-	})
+    dArray.append(1)
+    try {
+      dArray.pop(2)
+    } catch (e) {
+      assert.equal(e instanceof Error, true)
+    }
+  })
 
-	test('insert(i, x) - should throw an error if index is out of bounds', () => {
-		const dArray = new DynamicArray()
-		dArray.append(1)
-		try {
-			dArray.insert(2, 5)
-		} catch (e) {
-			expect(e).toBeInstanceOf(Error)
-		}
-	})
+  test('pop(i) - should shrink the underline array to save space', () => {
+    const dArray = new DynamicArray()
+    for (let i = 0; i < 10; i++) {
+      dArray.append(i + 1)
+    }
+    assert.deepEqual(dArray.__base, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, undefined, undefined, undefined, undefined, undefined, undefined])
+    for (let i = 0; i < 6; i++) {
+      dArray.pop(0)
+    }
+    assert.deepEqual(dArray.__base, [7, 8, 9, 10, undefined, undefined, undefined, undefined])
+  })
 
-	test('remove(x) - should remove the first occurrence of x from the array', () => {
-		const dArray = new DynamicArray()
-		dArray.append(1)
-		dArray.append(2)
-		dArray.append(2)
-		dArray.append(3)
-		dArray.remove(2)
-		expect(dArray.__base).toBe([1, 2, 3])
-	})
+  test('contains(x) - should return true if the array contains x, false otherwise', () => {
+    const dArray = new DynamicArray()
+    dArray.append(1)
+    dArray.append(2)
+    dArray.append(3)
+    assert.strictEqual(dArray.contains(2), true)
+    assert.strictEqual(dArray.contains(4), false)
+  })
+
+  test('insert(i, x) - should insert x at index i', () => {
+    const dArray = new DynamicArray()
+    dArray.append(1)
+    assert.deepEqual(dArray.__base, [1])
+    dArray.insert(0, 3)
+    assert.deepEqual(dArray.__base, [3, 1])
+    dArray.insert(1, 2)
+    assert.deepEqual(dArray.__base, [3, 2, 1, undefined])
+  })
+
+  test('insert(i, x) - should throw an error if index is out of bounds', () => {
+    const dArray = new DynamicArray()
+    dArray.append(1)
+    try {
+      dArray.insert(2, 5)
+    } catch (e) {
+      assert.strictEqual(e instanceof Error, true)
+    }
+  })
+
+  test('remove(x) - should remove the first occurrence of x from the array', () => {
+    const dArray = new DynamicArray()
+    for (let i = 0; i < 9; i++) {
+      dArray.append(1)
+    }
+    dArray.set(1, 2)
+    dArray.append(2)
+    assert.deepEqual(dArray.__base, [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, undefined, undefined, undefined, undefined, undefined, undefined])
+    dArray.remove(2)
+    assert.deepEqual(dArray.__base, [1, 1, 1, 1, 1, 1, 1, 1, 2, undefined, undefined, undefined, undefined, undefined, undefined, undefined])
+    dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 1, 1, 1, 1, 1, 1, 2, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined])
+		dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 1, 1, 1, 1, 1, 2, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined])
+		dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 1, 1, 1, 1, 2, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined])
+		dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 1, 1, 1, 2, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined])
+		dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 1, 1, 2, undefined, undefined, undefined, undefined])
+		dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 1, 2, undefined, undefined, undefined, undefined, undefined])
+		dArray.remove(1)
+    assert.deepEqual(dArray.__base, [1, 2, undefined, undefined])
+  })
 })
